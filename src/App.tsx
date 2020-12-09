71137de65d34ad97afb9dca1de34a79e7ae92301
Andrey Borovoi <andreyborovoi@mail.ru>
2020-12-09 19:58:45 +0300

    AN-4 #comment add ChatList comp

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,44 @@ import cn from 'classnames';
 import { Chat } from './components/Chat';
 import { ChatList } from './components/ChatList';
 
+import { chatInterface } from './interfaces';
+
 import styles from './App.module.scss';
+
+let Chats: Array<chatInterface> = [
+  { withUser: 'anonim123', lastMessage: 'q', fromThisUser: true, id: 1 },
+  { withUser: 'anon', lastMessage: 'asd', fromThisUser: false, id: 2 },
+  {
+    withUser: 'anton',
+    lastMessage: 'qwwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
+    fromThisUser: true,
+    id: 3,
+  },
+  { withUser: 'zxc', lastMessage: 'q', fromThisUser: false, id: 4 },
+  { withUser: 'anonim', lastMessage: 'q', fromThisUser: true, id: 5 },
+  { withUser: 'anon123', lastMessage: 'q', fromThisUser: false, id: 6 },
+];
 
 function App() {
   const [mobileMod, setMobileMod] = useState(true);
-  const [chatListSelected, setChatListSelected] = useState(true);
+  const [selectedChatId, setSelectedChatId] = useState(0);
 
   useEffect(() => {
     function onResize() {
-      if (window.innerWidth < 900) {
+      if (window.innerWidth < 700) {
         setMobileMod(true);
       } else {
         setMobileMod(false);
       }
     }
+    onResize();
     window.addEventListener('resize', onResize);
     return () => {
       window.removeEventListener('resize', onResize);
     };
   }, []);
+
+  useEffect(() => {}, [selectedChatId]);
 
   return (
     <div
@@ -31,14 +50,14 @@ function App() {
         [styles.mainWindowDesc]: !mobileMod,
       })}>
       {mobileMod ? (
-        chatListSelected ? (
-          <ChatList mobileMod={mobileMod} setChatListSelected={setChatListSelected} />
+        !selectedChatId ? (
+          <ChatList setSelectedChatId={setSelectedChatId} chatList={Chats} />
         ) : (
           <Chat />
         )
       ) : (
         <>
-          <ChatList mobileMod={mobileMod} />
+          <ChatList setSelectedChatId={setSelectedChatId} chatList={Chats} />
           <Chat />
         </>
       )}
--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import styles from "./ChatList.module.scss";
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Button from '@material-ui/core/Button';
+import PersonIcon from '@material-ui/icons/Person';
 
-export interface props {
-  mobileMod: Boolean;
-  setChatListSelected?: Function;
+import { chatInterface } from '../../interfaces';
+
+import styles from './ChatList.module.scss';
+
+interface props {
+  setSelectedChatId: Function;
+  chatList: Array<chatInterface>;
 }
 
-function ChatList({ mobileMod, setChatListSelected }: props) {
-  return <div className={styles.main}></div>;
+function ChatList({ setSelectedChatId, chatList }: props) {
+  return (
+    <div className={styles.main}>
+      <div className={styles.head}>
+        <IconButton>
+          <AddCircleIcon style={{ width: 30, height: 30 }} />
+        </IconButton>
+        <div>Начать новый диалог</div>
+      </div>
+      <div className={styles.list}>
+        {chatList.map((value) => {
+          return (
+            <Button key={value.id} onClick={() => setSelectedChatId(value.id)}>
+              <div className={styles.item}>
+                <div className={styles.iconWrap}>
+                  <PersonIcon style={{ width: 28, height: 28 }} />
+                </div>
+                <div className={styles.text}>
+                  <div className={styles.withUser}>{value.withUser}</div>
+                  <div className={styles.lastMessage}>
+                    {value.fromThisUser ? 'Вы: ' : ''}
+                    {value.lastMessage}
+                  </div>
+                </div>
+              </div>
+            </Button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export { ChatList };
